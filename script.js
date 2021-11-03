5ba26f1a3f47c5e5918eec53c4525e8d269a8335
Shiki-Himura <athaus@hotmail.de>
2021-11-03 12:09:37 +0100

    finish styling index and game add external jquery file implement coordinates to save data

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-$("gamebtn").on("click", function(event) {
-    // do stuff here c:
-    // magic *o*
+$(function() {
+    var playerOne = true;
+    var playField = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
+
+    $('.gamebtn').on('click', function(){
+        var x = $(this).data('x');
+        var y = $(this).data('y');
+
+        if(playerOne == true)
+        {
+            $(this).text("X");
+            playField[x][y] = 1;
+
+            $(this).css('background-color', '#55ff55');
+        }
+        else
+        {
+            $(this).text("O");
+            playField[x][y] = 2;
+
+            $(this).css('background-color', '#ff5555');
+        }
+        playerOne = !playerOne;
+        $(this).prop('disabled', true);
+    });
+
 });
