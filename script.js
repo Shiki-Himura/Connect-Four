8ae4e9e521da7dda3e97d9b0d5f0247a1d756a1f
Shiki-Himura <athaus@hotmail.de>
2021-11-10 14:53:39 +0100

    refactor tic-tac-toe to connect-4 update CSS accordingly implement functionality on mouseenter, mouseleave and click

--- script.js
+++ script.js
@@ -9,46 +9,33 @@ $(function() {
         [0,0,0,0,0,0,0]
     ];
 
-    $('.gamebtn').on('click', function(){
-        var x = $(this).data('x');
-        var y = $(this).data('y');
+    // listeners
+    const $board = $('#connect-four');
 
-        if(playerOne == true)
+    function findLastEmptyCell(col){
+        const cells = $(`.col .gamebtn[data-y='${col}']`);
+        for(let i = cells.length - 1; i >= 0; i--)
         {
-            playField[x][y] = 1;
-            $(this).text("X");
-            $(this).css('background-color', '#000000');
-            $(this).css('color', '#ffffff');
-
-            console.log(x, y);
-
-            dropChip();
-            checkWin();
+            const $cell = $(cells[i]);
+            if($cell.hasClass('empty'))
+                return $cell;
         }
-        else
-        {
-            playField[x][y] = 2;
-            $(this).text("O");
-            $(this).css('background-color', '#ffffff');
-            $(this).css('color', '#000000');
+        return null;
+    }
 
-            dropChip();
-            checkWin();
-        }
-        playerOne = !playerOne;
-        $(this).prop('disabled', true);
+    $board.on('mouseenter', '.col .empty', function() {
+        const col = $(this).data('y');
+        const $lastEmptyCell = findLastEmptyCell(col);
+        $lastEmptyCell.addClass(`next-red`);
     });
 
-    function dropChip()
-    {
-        // check for empty columns
-        // add class .fall to element
-        // let chip be placed at the bottom
-    }
+    $board.on('mouseleave', '.col .gamebtn', function() {
+        $('.col .gamebtn').removeClass(`next-red`);
+    });
 
-    function checkWin()
-    {
-        // check possible win situations
-        // implement alpha-pruning to increase performance
-    }
+    $board.on('click', '.col .empty', function() {
+        const col = $(this).data('y');
+        const $lastEmptyCell = findLastEmptyCell(col);
+        $lastEmptyCell.removeClass('empty').addClass('red');
+    });
 });
